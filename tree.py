6fef9f065a15714c6a580c64e3e0ba3d01749293
GQLIATH <147947920+GQLIATH@users.noreply.github.com>
2025-12-09 11:12:21 +0300

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self,key):
+        self.key=key
+        self.left=None
+        self.right=None
+def inorder (root):
+      if root is not None:
+       inorder(root.left)
+       print(str(root.key),end="-> ")
+       inorder(root.right)
+def preorder (root):
+      if root is not None:
+       print(str(root.key),end="-> ")
+       preorder(root.left)
+       
+       preorder(root.right)
+
+def postorder (root):
+      if root is not None:
+       postorder(root.left)
+       postorder(root.right)
+       print(str(root.key),end="-> ")
+
+nodes=[10,20,30,40,50]
+print("\n nodes in the binary tree : ",nodes)
+root=Node(nodes[0])
+root.left=Node(nodes[1])
+root.right=Node(nodes[2])
+root.left.left=Node(nodes[3])
+root.left.right=Node(nodes[4])
+print("\n inorder traversal: ",end=" ")
+inorder(root)
+print("\npreorder traversal: ",end=" ")
+preorder(root)
+print("\npostorder traversal: ",end=" ")
+postorder(root)
